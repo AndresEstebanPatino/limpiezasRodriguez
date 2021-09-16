ca510ce3f25a42f71bc3400440123b42ae5a29f9
Andres Patiño <andresp199519@gmail.com>
2021-09-16 13:05:29 +0200

    Se cambió la imagen del main del home, se puso el copy de esa mmism sección, y se pusieron os enlaces a precios y a contacto.

---
+++ limpiezasRodriguez/assets/js/hover.js
@@ -0,0 +1,18 @@
+
+let hover = document.getElementById("color-icono-hover");
+
+
+hover.addEventListener('mouseover', function() {
+
+document.getElementById("icono").className +="color-icono"
+   
+});
+
+
+
+
+    hover.addEventListener('mouseenter'), function(event){
+        event.target.style.color = "white"
+        console.log("hola")
+    }
+  
